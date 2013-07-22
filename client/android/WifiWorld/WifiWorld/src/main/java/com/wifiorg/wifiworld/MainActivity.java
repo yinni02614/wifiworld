1a059f7ae0a64f20929da2acfdafca267935c72f
peigong <peigong@foxmail.com>
2013-07-22 18:22:55 +0800

    完成APP和服务器HTML联动的DEMO。

--- client/android/WifiWorld/WifiWorld/src/main/java/com/wifiorg/wifiworld/MainActivity.java
+++ client/android/WifiWorld/WifiWorld/src/main/java/com/wifiorg/wifiworld/MainActivity.java
@@ -2,18 +2,29 @@ package com.wifiorg.wifiworld;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.view.KeyEvent;
 import android.view.Menu;
 import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
 public class MainActivity extends Activity {
+    private  WebView view;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        //setContentView(R.layout.activity_main);
-        WebView view = new WebView(this);
-        this.setContentView(view);
-        view.loadUrl("http://www.peigong.tk");
+        setContentView(R.layout.activity_main);
+        this.view = (WebView)this.findViewById(R.id.mainWebView);
+        this.view.getSettings().setJavaScriptEnabled(true);
+        this.view.getSettings().setLoadsImagesAutomatically(true);
+        this.view.getSettings().setBuiltInZoomControls(true);
+        this.view.loadUrl("http://www.peigong.tk");
+        this.view.setWebViewClient(new WebViewClient(){
+            public boolean shouldOverrideUrlLoading(WebView view, String url) {
+                view.loadUrl(url);
+                return true;
+            }
+        });
     }
 
 
@@ -23,5 +34,11 @@ public class MainActivity extends Activity {
         getMenuInflater().inflate(R.menu.main, menu);
         return true;
     }
-    
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        if ((keyCode == KeyEvent.KEYCODE_BACK) && this.view.canGoBack()) {
+            this.view.goBack();
+            return true;
+        }
+        return super.onKeyDown(keyCode, event);
+    }
 }
