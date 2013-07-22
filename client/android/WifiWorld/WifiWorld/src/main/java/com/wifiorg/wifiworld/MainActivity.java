ffc6fef4026c855b8642ae11d6c681fb8b85a71f
peigong <peigong@foxmail.com>
2013-07-22 13:40:22 +0800

    增加android程序。

---
+++ client/android/WifiWorld/WifiWorld/src/main/java/com/wifiorg/wifiworld/MainActivity.java
@@ -0,0 +1,27 @@
+package com.wifiorg.wifiworld;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+import android.webkit.WebView;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        //setContentView(R.layout.activity_main);
+        WebView view = new WebView(this);
+        this.setContentView(view);
+        view.loadUrl("http://www.peigong.tk");
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+    
+}
